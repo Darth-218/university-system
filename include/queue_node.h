5f599e08f85acd6d0368b33cc9003a2ac026e098
Asurate <ahmedmaboud2005@gmail.com>
2025-01-17 15:05:20 +0200

    created header file for queue node

---
+++ include/queue_node.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+template <typename T> class Node {
+public:
+  T value;
+  Node *next;
+
+  Node(T value) {
+    this->value = value;
+    next = NULL;
+  }
+};
