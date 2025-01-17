87efda9224d1e5a32e797f54dee6dd5611844d8b
Asurate <ahmedmaboud2005@gmail.com>
2025-01-17 15:04:59 +0200

    created header file for queue

---
+++ include/queue.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "queue_node.h"
+using namespace std;
+
+template <typename T> class Queue {
+private:
+  Node<T> *rear, *front;
+  int size;
+
+public:
+  Queue();
+
+  bool isEmpty();
+
+  bool enqueue(T value);
+
+  int getSize();
+
+  bool dequeue();
+
+  void peek();
+};
+
+#include "../templates/queue.tpp"
