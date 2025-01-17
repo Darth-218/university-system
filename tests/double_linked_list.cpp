2a0a656681a60225589c7ebea7c2c4e4c155a248
Asurate <ahmedmaboud2005@gmail.com>
2025-01-17 15:07:09 +0200

    Update double_linked_list.cpp

--- tests/double_linked_list.cpp
+++ tests/double_linked_list.cpp
@@ -1,12 +1,15 @@
-#include "../include/double_linked_list.h"
+#include "../include/doubly_linked_list.h"
 
 using namespace std;
 
 int main() {
-  DoubleLinkedList<int> dll;
+  DoublyLinkedList<int> dll;
 
   dll.push(4);
   dll.push(5);
+  dll.push(6);
+  dll.push(7);
+  dll.push(8);
 
   dll.append(9);
 
@@ -22,5 +25,11 @@ int main() {
   dll.display();
 
   dll.removeHead();
+  dll.display();
+
+  dll.removeTail();
+  dll.display();
+
+  dll.deleteNode(4);
   dll.display();
 }
