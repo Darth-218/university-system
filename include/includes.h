b6e1417b3e88cb42732f17d23844f20960627db3
darth-218 <yahia.hany.gaber@proton.me>
2025-01-19 20:16:49 +0200

    Created two files to manage includes

---
+++ include/includes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstddef>
+#include <iostream>
+#include <optional>
+#include <string>
+
+using namespace std;
---
+++ include/structures.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "binary_tree.h"
+#include "doubly_linked_list.h"
+#include "hash_table.h"
+#include "queue.h"
+#include "singly_linked_list.h"
+#include "stack.h"
